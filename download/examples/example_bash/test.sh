ac1dcd10acabe1645d50c314aff501875fc82be2
Robrecht Cannoodt <rcannood@gmail.com>
2023-02-09 12:02:36 +0100

    update unit test

--- download/examples/example_bash/test.sh
+++ download/examples/example_bash/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo ">>> Creating input test file"
+echo ">>> Create input test file"
 echo "foo" > foo.txt
 
-echo ">>> Running executable"
+echo ">>> Run executable"
 $meta_executable --input foo.txt --output bar.txt
 
 echo ">>> Check whether output file exists"
-[[ ! -f bar.txt ]] && echo "Output file could not be found!" && exit 1
+[[ -f bar.txt ]] || (echo "Output file could not be found!" && exit 1)
 
 echo ">>> Check whether input and output file are the same"
 cmp foo.txt bar.txt || (echo "Input and output files are different!" && exit 1)
