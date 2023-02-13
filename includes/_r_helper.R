e34fd16b697691a28e587c8064c2aac7597af6c8
Robrecht Cannoodt <rcannood@gmail.com>
2023-02-13 14:47:59 +0100

    remove run_cmd for now

--- includes/_r_helper.R
+++ includes/_r_helper.R
@@ -11,33 +11,33 @@ strip_margin <- function(str, margin = "\\|") {
   gsub(regex, "\\1", str)
 }
 
-# run command and display as a mardown codeblock
-# `...` doesn't work, for some reason
-run_cmd <- function(command, args = character(), wd = NULL, error_on_status = TRUE, stderr_to_stdout = FALSE) {
-  # run command
-  cmd_out <- processx::run(
-    command = command,
-    args = args,
-    wd = wd,
-    error_on_status = error_on_status,
-    stderr_to_stdout = stderr_to_stdout
-  )
+# # run command and display as a mardown codeblock
+# # `...` doesn't work, for some reason
+# run_cmd <- function(command, args = character(), wd = NULL, error_on_status = TRUE, stderr_to_stdout = FALSE) {
+#   # run command
+#   cmd_out <- processx::run(
+#     command = command,
+#     args = args,
+#     wd = wd,
+#     error_on_status = error_on_status,
+#     stderr_to_stdout = stderr_to_stdout
+#   )
 
-  # indent output
-  out_indented <-
-    strsplit(cmd_out$stdout, "\n")[[1]] %>%
-    paste0("    ", .) %>%
-    paste(., collapse = "\n")
+#   # indent output
+#   out_indented <-
+#     strsplit(cmd_out$stdout, "\n")[[1]] %>%
+#     paste0("    ", .) %>%
+#     paste(., collapse = "\n")
   
-  # format as markdown
-  paste0(
-    quo, "bash\n",
-    command, " ", paste(args, collapse = " "), "\n",
-    quo, "\n",
-    "\n",
-    out_indented, "\n"
-  )
-}
+#   # format as markdown
+#   paste0(
+#     quo, "bash\n",
+#     command, " ", paste(args, collapse = " "), "\n",
+#     quo, "\n",
+#     "\n",
+#     out_indented, "\n"
+#   )
+# }
 
 # render code as qmd and display as markdown
 run_quarto <- function(src_qmd, wdir, engine = "knitr") {
@@ -61,7 +61,7 @@ run_quarto <- function(src_qmd, wdir, engine = "knitr") {
   # write qmd source code
   readr::write_lines(src_and_header, file_qmd)
 
-  # ender to markdown
+  # render to markdown
   out <- processx::run(
     command = "quarto",
     args = c(
