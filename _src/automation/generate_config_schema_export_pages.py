05de3fa05af899a28c9056eac068dbb1a7b3f05e
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2023-03-02 10:32:24 +0100

    Greatly simplify page creation
    Don't split pages into separate chunks for each variable key, throw them on a heap and then collect them back into pages
    Now we take a subsection from the yaml, sort the keys and directly create the page from there
    
    Arguments are no longer collected into a single big page but instead create separate pages for all requirements.
    Still need to create an overview landing page, maybe use the general requirements for that?

--- _src/automation/generate_config_schema_export_pages.py
+++ _src/automation/generate_config_schema_export_pages.py
@@ -12,30 +12,29 @@ keyword_regex = r"\@\[(.*?)\]\((.*?)\)"
 reference_dir = repo_root + "/reference/"
 config_dir = reference_dir + "/config"
 
-config_file = Path(repo_root, '_src', 'automation', 'config_pages_settings.yaml')
-config_pages_settings = ''
-with open(config_file, 'r') as infile:
-		config_pages_settings = yaml.safe_load(infile)
-
 def generate_json():
 	""" Calls viash in order to generate a config export. """
 	
 	# Run bin/viash export config_schema
 	json = subprocess.run(["viash", "export", "config_schema"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-	f = open(reference_dir + json_export, "w")
-	f.write(json)
-	f.close()
+	json_file = Path(repo_root, "reference", json_export)
+	with open(json_file, 'w') as outfile:
+		outfile.write(json)
 
 	print(f"Generated {reference_dir}/{json_export}")
 
+def read_config_page_settings():
+	global config_pages_settings
+	config_file = Path(repo_root, '_src', 'automation', 'config_pages_settings.yaml')
+	with open(config_file, 'r') as infile:
+			config_pages_settings = yaml.safe_load(infile)
 
 def read_json_entries():
 	""" Load the generated JSON file and pass the entries to be read by the get_json_entries function. """
 
-	reference_dir = repo_root + "/reference/"
-	json_file = open(reference_dir + json_export, "r")
-	viash_json = json.load(json_file)
-	json_file.close()
+	json_file = Path(repo_root, "reference", json_export)
+	with open(json_file, 'r') as infile:
+		viash_json = json.load(infile)
 
 	for topic in viash_json:
 		if isinstance(viash_json[topic], dict):
@@ -56,75 +55,28 @@ def get_json_entries(page_title, json_entry, topic):
 	# Sort json entries alphabetically and store in a dictionary
 	sorted_dict = sorted(json_entry, key=lambda x: x["name"], reverse=False)
 
+	page_data = {}
+	page_data['topic'] = topic
+	page_data['title'] = title
+	page_data['data'] = []
+
 	for newData in sorted_dict:
-		newData['topic'] = topic
-		newData['title'] = title
 		if 'description' in newData:
 			newData['description'] = replace_keywords(newData["description"])
+		page_data['data'].append(newData)
 
-		if not topic in entry_lists:
-			entry_lists[topic] = []
-		entry_lists[topic].append(newData)
-
-def generate_pages():
-	"""Feed JsonEntryData lists to page generation functions."""
-
-	generate_combined_page(entry_list= entry_lists['functionality'], page_title= "Functionality", filename= "functionality", title_is_name=False)
-	generate_combined_page(entry_list= entry_lists['requirements'], page_title= "Setup Requirements", filename= "requirements", title_is_name=True)
-	generate_grouped_pages(entry_list= entry_lists['platforms'], group_name= "platforms")
-	generate_grouped_pages(entry_list= entry_lists['arguments'], group_name= "arguments")
-
-def generate_combined_page(entry_list, page_title, filename, title_is_name):
-	"""
-	Parses a data dictionary and combines it into a single page.
-
-	Arguments:
-		 entry_list: List of dicts to parse
-		 page_title: Title of the combined page
-		 save_filename: Name of the file without any extensions
-		 title_is_name: If True, the title property of every dict will be used as if it was the name property
-	"""
-
-	entry_list = sorted(entry_list, key=lambda x: x['title'], reverse=False)
-
-	page_data = {}
-	page_data['pageTitle'] = page_title
-	page_data['title_is_name'] = title_is_name
-	page_data['data'] = entry_list
+	filename = topic
+	if topic != page_title:
+		filename = f"{topic}/{page_title}"
 
 	if filename in config_pages_settings['structure']:
 		filename = config_pages_settings['structure'][filename]
 	else:
 		print(f"Could not find {filename} in the config pages settings structure")
 		
-	render_jinja_page("combined_page.j2.qmd", config_dir, filename, page_data)
-
-def generate_grouped_pages(entry_list, group_name):
-	"""
-	Parses a list of data dictionaries, groups them by title and creates a page for each title.
-
-	Arguments:
-		 entry_list: List of dicts to parse
-		 save_dir: Subdirectory of config to place files. Will be created if it doesn't exist.
-	"""
-
-	# Group entries per title
-	uniqueTitles = set(map(lambda x:x['title'], entry_list))
-	groupedList = [[y for y in entry_list if y['title']==x] for x in uniqueTitles]
-
-	for group in groupedList:
-		page_data = {}
-		page_data['pageTitle'] = group[0]['title']
-		# page_data['title_is_name'] = title_is_name
-		page_data['data'] = group
-		
-		filename = group_name + "/" + group[0]['title'].replace(" ", "")
-
-		if filename in config_pages_settings['structure']:
-			filename = config_pages_settings['structure'][filename]
-		else:
-			print(f"Could not find {filename} in the config pages settings structure")
-
+	if topic == 'functionality' or topic == 'requirements':
+		render_jinja_page("combined_page.j2.qmd", config_dir, filename, page_data)
+	else:
 		render_jinja_page("grouped_page.j2.qmd", config_dir, filename, page_data)
 	
 def clean_title(title) -> str:
@@ -142,13 +94,12 @@ def render_jinja_page(template: str, folder: str, filename: str, data: dict):
 
 	yaml_file = Path(folder, filename).with_suffix('.yaml')
 	qmd_file = Path(folder, filename).with_suffix('.qmd')
+	template_file = Path(repo_root, "_src" ,"automation", template)
 	
 	with open(yaml_file, 'w') as outfile:
 			yaml.safe_dump(data, outfile, default_flow_style=False)
 
-	path_template = Path(repo_root, "_src" ,"automation", template)
-	
-	qmd = subprocess.run(["j2", path_template, yaml_file], stdout=subprocess.PIPE).stdout.decode('utf-8')
+	qmd = subprocess.run(["j2", template_file, yaml_file], stdout=subprocess.PIPE).stdout.decode('utf-8')
 	with open(qmd_file, 'w') as outfile:
 		outfile.write(qmd)
 
@@ -173,5 +124,5 @@ def replace_keywords(text: str) -> str:
 
 if __name__ == "__main__":
 	generate_json()
+	read_config_page_settings()
 	read_json_entries()
-	generate_pages()
