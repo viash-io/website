d609f0d7abf3b49171d5f041eb38294f24a3193a
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2023-03-02 20:39:07 +0100

    some formatting tweaks and old code purge

--- _src/automation/generate_cli_schema_export_pages.py
+++ _src/automation/generate_cli_schema_export_pages.py
@@ -14,9 +14,6 @@ template_file = Path(repo_root, "_src" ,"automation", "template_cli_page.j2.qmd"
 reference_dir = repo_root + "/reference/"
 
 config_file = Path(repo_root, '_src', 'automation', 'config_pages_settings.yaml')
-config_pages_settings = ''
-with open(config_file, 'r') as infile:
-		config_pages_settings = yaml.safe_load(infile)
 
 def generate_json():
 	""" Calls viash in order to generate a cli export. """
@@ -47,97 +44,24 @@ def generate_pages():
 
 
 def create_page(name, json_data):
-	# qmd = ""
-	# qmd += qmd_header(f"viash {name}")
-
-	# if "bannerCommand" in json_data:  # Info is in top level command
-	# 	qmd += qmd_add_command(command_json=json_data, is_subcommand=False)
-	# else:  # Info is in subcommands
-	# 	for subcommand_json in json_data["subcommands"]:
-	# 		qmd += qmd_add_command(command_json=subcommand_json, is_subcommand=True)
-
-	# write_qmd_file("cli", name, qmd)
 
 	if "bannerCommand" in json_data: # Info is in top level command
 		page_data = {'title': f'viash {name}', 'usesSubcommands': False, 'data': [json_data]}
 	else:
 		page_data = {'title': f'viash {name}', 'usesSubcommands': True, 'data': json_data['subcommands']}
 
+	# TODO: description = replace_keywords(argument["descr"])
+
 	render_jinja_page(cli_dir, name, page_data)
 
-def qmd_add_command(command_json, is_subcommand) -> str:
-	""" Returns the information about the command in markdown form. """
-	qmd = ""
-	
-	if is_subcommand: # This is a subcommand, so add a H2 with its name
-		qmd += qmd_h2(command_json["bannerCommand"])
-
-	qmd += command_json["bannerDescription"] + "\n\n"
-	qmd += qmd_bold_paragraph("Usage:")
-	qmd += qmd_code_paragraph(command_json["bannerUsage"])
-	qmd += qmd_create_arguments_table(command_json["opts"])
-
-	return qmd
-
-def qmd_header(title) -> str:
-	""" Returns the page metadata markdown that belongs at the top of a qmd file, with the title filled in. """
-	qmd = "---\n"
-	qmd += f"title: {title}\n"
-	qmd += "search: true\n"
-	qmd += "execute:\n"
-	qmd += "  echo: false\n"
-	qmd += "  output: asis\n"
-	qmd += "---\n\n"
-	return qmd
-
-def qmd_code_paragraph(text) -> str:
-	""" Returns text with newlines and code styling added. """
-	qmd = ""
-	splitted = text.split("\n")
-	for part in splitted:
-		qmd += "`" + part + "`\n\n"
-	return qmd
-
-def qmd_bold_paragraph(text) -> str:
-	""" Returns text with newlines and bold styling added. """
-	return "**" + text + "**\n\n"
-
-def qmd_h2(text) -> str:
-	""" Returns text with H2 markdown and newlines. """
-	return "## " + text + "\n\n"
-
-def qmd_create_arguments_table(arguments_list) -> str:
-	""" Creates a table of arguments found in the given arguments_list """
-	qmd = ""
-	qmd += "| Argument | Description | Type |\n"
-	qmd += "|-|:----|-:\n"
-
-	sorted_arguments = sorted(arguments_list, key=lambda x: x["name"], reverse=False)
-
-	for argument in sorted_arguments:
-		if argument['name'] == "config":
-			argument_name = f"`{argument['name']}`"
-		else:
-			argument_name = f"`--{argument['name']}`"
-
-		if "short" in argument:
-			argument_name += f", `-{argument['short']}`"
-
-		description = replace_keywords(argument["descr"])
-		description = description.replace(
-			"$", "\$"
-		)  # Prevents dollar sign being detected as LaTeX start
-
-		if argument["required"]:
-			description += " **This is a required argument.**"
-
-		qmd += f"| {argument_name} | {description} | `{argument['type']}` |\n"
-
-	# Always add --help
-	qmd += f"| `--help`, `-h` | Show help message |  |\n"
-
-	qmd += "\n\n"
-	return qmd
+# TODO
+# def qmd_code_paragraph(text) -> str:
+# 	""" Returns text with newlines and code styling added. """
+# 	qmd = ""
+# 	splitted = text.split("\n")
+# 	for part in splitted:
+# 		qmd += "`" + part + "`\n\n"
+# 	return qmd
 
 def render_jinja_page(folder: str, filename: str, data: dict):
 	""" Write data to yaml file and run jinja. """
@@ -176,12 +100,6 @@ def replace_keywords(text: str) -> str:
 		text = text.replace(whole_match, f"[{keyword_text}]({link})")
 
 	return text
-
-def write_qmd_file(dir_in_reference, file_name, content):
-	Path(reference_dir + f"{dir_in_reference}/").mkdir(parents=True, exist_ok=True)
-	qmd_file = open(reference_dir + f"{dir_in_reference}/{file_name}.qmd", "w")
-	qmd_file.write(content)
-	qmd_file.close()
 
 if __name__ == "__main__":
 	generate_json()
