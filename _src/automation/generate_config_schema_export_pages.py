3d057568a4000701bb1143d9d562f56f5470ac1d
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2023-03-02 12:30:22 +0100

    Some general clean-up and reshuffling of some arguments or operations

--- _src/automation/generate_config_schema_export_pages.py
+++ _src/automation/generate_config_schema_export_pages.py
@@ -1,4 +1,4 @@
-import git, subprocess, json, csv, re, yaml, jinja2
+import git, subprocess, json, re, yaml
 from pathlib import Path
 
 entry_lists = {}
@@ -23,13 +23,15 @@ def generate_json():
 	print(f"Generated {json_file}")
 
 def read_config_page_settings():
+	""" Load the folder structure and keyword replacements file. """
+
 	global config_pages_settings
 	config_file = Path(repo_root, '_src', 'automation', 'config_pages_settings.yaml')
 	with open(config_file, 'r') as infile:
 			config_pages_settings = yaml.safe_load(infile)
 
 def read_json_entries():
-	""" Load the generated JSON file and pass the entries to be read by the get_json_entries function. """
+	""" Load the generated JSON file, split into logical page chunks and generate pages. """
 
 	with open(json_file, 'r') as infile:
 		viash_json = json.load(infile)
@@ -37,36 +39,32 @@ def read_json_entries():
 	for topic in viash_json:
 		if isinstance(viash_json[topic], dict):
 			for subtopic in viash_json[topic]:
-				get_json_entries(subtopic = subtopic, topic = topic, json_entry = viash_json[topic][subtopic])
+				generate_page(topic, subtopic, viash_json[topic][subtopic])
 		else:
-			get_json_entries(subtopic= topic, topic = topic, json_entry = viash_json[topic])		
+			generate_page(".", topic, viash_json[topic])
 
-def get_json_entries(subtopic, json_entry, topic):
-	""" Reads the generated JSON file and extract all information into instances of the JsonEntryData class. """
+def generate_page(topic: str, subtopic: str, json_data: dict):
+	""" Receives JSON data, does some minor data manipulation and writes to yaml & qmd. """
 
 	if topic == 'arguments': # Keep title of argument pages as-is
 		title = subtopic
 	else:
 		title = re.sub(r"(\w)([A-Z])", r"\1 \2", subtopic).title() # split words and capitalize
 
-	# Sort json entries alphabetically and store in a dictionary
-	sorted_dict = sorted(json_entry, key=lambda x: x["name"], reverse=False)
+	# Sort data entries alphabetically on 'name'
+	sorted_dict = sorted(json_data, key=lambda x: x["name"], reverse=False)
+
+	# Fix description markdown keywords to links
+	for d in sorted_dict:
+		if 'description' in d:
+			d['description'] = replace_keywords(d["description"])
 
 	page_data = {}
 	page_data['topic'] = topic
 	page_data['title'] = title
-	page_data['data'] = []
+	page_data['data'] = sorted_dict
 
-	for newData in sorted_dict:
-		# Fix our markdown identifiers to proper markdown links
-		if 'description' in newData:
-			newData['description'] = replace_keywords(newData["description"])
-		page_data['data'].append(newData)
-
-	if topic != subtopic:
-		filename = f"{topic}/{subtopic}"
-	else:
-		filename = topic
+	filename = f"{topic}/{subtopic}"
 
 	if filename in config_pages_settings['structure']:
 		filename = config_pages_settings['structure'][filename]
@@ -86,25 +84,27 @@ def render_jinja_page(folder: str, filename: str, data: dict):
 	base_dir.mkdir(parents=True, exist_ok=True)	
 	
 	with open(yaml_file, 'w') as outfile:
-			yaml.safe_dump(data, outfile, default_flow_style=False)
+		yaml.safe_dump(data, outfile, default_flow_style=False)
 
 	qmd = subprocess.run(["j2", template_file, yaml_file], stdout=subprocess.PIPE).stdout.decode('utf-8')
 	with open(qmd_file, 'w') as outfile:
 		outfile.write(qmd)
 
 def replace_keywords(text: str) -> str:
-	""" Finds all keywords in the format @[keyword](text) and returns the replacements based on the keywords CSV. """
+	""" Finds all keywords in the format @[keyword](text) and returns the replacements based on the keywords settings. """
+
 	# Find all keyword links
 	matches = re.finditer(keyword_regex, text, re.MULTILINE)
 
 	for matchNum, match in enumerate(matches, start=1):
 		whole_match = match.group(0)
-		keyword = match.group(1)
-		keyword_text = match.group(2)
-		link = "no-link"
+		keyword, keyword_text = match.groups()
 
 		if keyword in config_pages_settings['keywords']:
 			link = config_pages_settings['keywords'][keyword]
+		else:
+			link = "no-link"
+			print(f"Could not find {keyword} in the config pages settings keywords")
 
 		# Replace match with hyperlink
 		text = text.replace(whole_match, f"[{keyword_text}]({link})")
