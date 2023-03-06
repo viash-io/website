b442f2d2f387a6e00150c88a392ec25502c07246
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2023-03-06 12:47:36 +0100

    Move cli and config scripts into viash components
    Adapt scripts to use viash parameters
    Create cli & config exports in the higher-up bash script instead of inside the script

--- _src/render_pages.sh
+++ _src/render_pages.sh
@@ -7,7 +7,11 @@ viash run _src/automation/generate_version_blog_pages/config.vsh.yaml -- \
   --output blog/posts
 
 echo "Creating cli information"
-python _src/automation/generate_cli_schema_export_pages.py
+viash export cli_schema --output ./reference/cli_schema_export.json
+viash run _src/automation/generate_reference_cli_pages/config.vsh.yaml -- \
+  --input ./reference/cli_schema_export.json
 
 echo "Creating config information"
-python _src/automation/generate_config_schema_export_pages.py
+viash export config_schema --output ./reference/config_schema_export.json
+viash run _src/automation/generate_reference_config_pages/config.vsh.yaml -- \
+  --input ./reference/config_schema_export.json
