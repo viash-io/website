568eba588ebe9692cd90ac5dcbbdf5255f272cf1
Robrecht Cannoodt <rcannood@gmail.com>
2023-03-17 09:07:09 +0100

    use slim containers

--- guide/component/_language_examples.R
+++ guide/component/_language_examples.R
@@ -8,10 +8,10 @@ repo_path <- system("git rev-parse --show-toplevel", intern = TRUE)
 langs <- tribble(
   ~id, ~label, ~script, ~script_type, ~container,
   "bash", "Bash", "script.sh", "bash_script", '"bash:4.0"',
-  "csharp", "C#", "script.csx", "csharp_script", '"dataintuitive/dotnet-script:1.2.1"\n    setup: [{ type: apk, packages: bash }]',
-  "js", "JavaScript", "script.js", "javascript_script", '"node:15-buster"',
-  "python", "Python", "script.py", "python_script", '"python:3.10"',
-  "r", "R", "script.R", "r_script", '"rocker/tidyverse:4.2"',
+  "csharp", "C#", "script.csx", "csharp_script", '"ghcr.io/data-intuitive/dotnet-script:1.3.1"',
+  "js", "JavaScript", "script.js", "javascript_script", '"node:19-bullseye-slim"',
+  "python", "Python", "script.py", "python_script", '"python:3.10-slim"',
+  "r", "R", "script.R", "r_script", '"eddelbuettel/r2u:22.04"',
   "scala", "Scala", "script.scala", "scala_script", '"sbtscala/scala-sbt:eclipse-temurin-19_36_1.7.2_2.13.10"'
 ) %>%
   mutate(
