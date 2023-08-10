326a4dea521c0cf1dffec55e4a4f9cdc8aa41633
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2023-08-10 13:11:08 +0200

    Copy static reference pages from viash
    - Remove all subfolders in reference
    - Copy static pages
    - Rebuild cli & config pages

--- _src/render_pages.sh
+++ _src/render_pages.sh
@@ -8,6 +8,12 @@ viash run _src/automation/generate_version_blog_pages/config.vsh.yaml -- \
 
 source renv/python/virtualenvs/renv-python-3.10/bin/activate
 
+echo "Removing reference subfolders but leaving top level files in place"
+rm -rf ./reference/*/
+
+echo "Copying static reference pages"
+cp -r ../viash/static_reference_pages/* ./reference
+
 echo "Creating cli information"
 viash export cli_schema --output ./reference/cli_schema_export.json --format json
 viash run _src/automation/generate_reference_cli_pages/config.vsh.yaml -- \
