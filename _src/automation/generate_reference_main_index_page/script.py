f1b467437d3f254a9064ca5fe4ef7c299e3ed76d
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2024-06-26 07:56:18 +0200

    Move functionality & platforms to a deprecated subfolder
    There are still a lot of broken links, in e.g. the guide
    The reference itself should be updated too to remove references to functionality and platforms

--- _src/automation/generate_reference_main_index_page/script.py
+++ _src/automation/generate_reference_main_index_page/script.py
@@ -43,22 +43,22 @@ def generate_reference_page():
 
 	# Add some static pages :(
 	add_entry("Viash Config", "Config Overview", "/reference/config/index")
-	add_entry("Viash Config", "Functionality", "/reference/config/functionality/index")
+	# add_entry("Viash Config", "Functionality", "/reference/config/functionality/index")
 	add_entry("Viash Config", "Runners", "/reference/config/runners/index")
 	add_entry("Viash Config", "Engines", "/reference/config/engines/index")
-	add_entry("Viash Config", "Platforms", "/reference/config/platforms/index")
-	add_entry("Miscellaneous", "Project Config", "/reference/project/index")
-	add_entry("Miscellaneous", "Environment Variables", "/reference/config/environmentVariables")
+	# add_entry("Viash Config", "Platforms", "/reference/config/platforms/index")
+	add_entry("Miscellaneous", "Package Config", "/reference/packageConfig/index")
+	add_entry("Miscellaneous", "Environment Variables", "/reference/environmentVariables/index")
 	add_entry("Miscellaneous", "Config Mods", "/reference/config_mods/index")
 	add_entry("Miscellaneous", "Viash Code Block", "/reference/viash_code_block/index")
 	
 	# Runners/Engines/Platforms are not simply contained in a main folder, grab index files. For other matches, skip the index files.
 	add_entries_from_glob('reference/config/runners/*/index.qmd', "Runners", False)
 	add_entries_from_glob('reference/config/engines/*/index.qmd', "Engines", False)
-	add_entries_from_glob('reference/config/platforms/*/index.qmd', "Platforms", False)
+	# add_entries_from_glob('reference/config/platforms/*/index.qmd', "Platforms", False)
 	add_entries_from_glob('reference/config/engines/docker/setup/*.qmd', "Docker Setup Requirements", True)
-	add_entries_from_glob('reference/config/functionality/arguments/*.qmd', "Argument Types", True)
-	add_entries_from_glob('reference/config/functionality/resources/*.qmd', "Resource Types", True)
+	add_entries_from_glob('reference/config/arguments/*.qmd', "Argument Types", True)
+	add_entries_from_glob('reference/config/resources/*.qmd', "Resource Types", True)
 	
 	# Minor final touch on the output and save to file
 	output = []
