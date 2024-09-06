c9b46f8de73cbad27c831b35ccc606c473f6b723
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2024-09-07 00:06:22 +0200

    0.9.0 release (#96)
    * Prepare runner & engine pages
    
    update scripts to use engines instead of platforms
    Create dummy blog page for WIP release notes. Substitute placeholders with values that get picked up by Quarto and place the file at the top.
    
    Dependencies were not (fully?) documented before, these should come through now too.
    
    * Commit updated files from running scripts
    
    * Update cheatsheet
    
    * Prepare some config changes for building version 0.9.0
    
    * Update page reorganization and remove prints
    
    * Inject notes in some descriptions about project config values being copied to a component config
    
    If we need to do this more often, we should consider adding a "description note" field in the annotations & use it in the jinja template
    
    * update website preparation for current state of the 0.9.0 branch
    
    Not including updated generated pages just yet
    
    * Rebuild renv after updating from 0.16.0 to 1.0.7
    
    * Update script and package config for release candidate
    
    * Run render_pages script and update 1 guide page to use `runner` instead of `platform`
    
    * Undo guide change and rebuild with default viash 0.8.6
    
    * Move functionality & platforms to a deprecated subfolder
    
    There are still a lot of broken links, in e.g. the guide
    The reference itself should be updated too to remove references to functionality and platforms
    
    * update reference documentation & config
    
    * Rebuild guide for latest changes in released viash version
    
    * updated example components for viash 0.9
    
    * bump R to 4.4
    
    * remove 0.9.0-RC versioned files
    
    * remove RC blog posts
    
    * use --runner instead of -p for a nextflow platform
    
    * updated component config modifications
    
    * Fix a series of broken links and minor guide fixes
    
    * create component and add dependencies pages updated
    
    * fix create component page
    
    * modified template branch, updated quickstart
    
    * PR fixes
    
    * Remove `.RData` and add it to .gitignore
    
    * trigger gha
    
    ---------
    
    Co-authored-by: jakubmajercik <jakub.majercik@gmail.com>

--- _src/automation/generate_reference_config_pages/script.py
+++ _src/automation/generate_reference_config_pages/script.py
@@ -44,6 +44,7 @@ def generate_page(json_data: list):
 		if d['name'] == '__this__':
 			this_parameter = d
 		if 'description' in d:
+			d['description'] = inject_project_config_copying_notes(this_parameter['niceType'], d['name'], d['description'])
 			d['description'] = replace_keywords(d["description"])
 
 	topic = this_parameter['type']
@@ -103,6 +104,34 @@ def replace_keywords(text: str) -> str:
 
 	return text
 
+def inject_project_config_copying_notes(class_name: str, parameter_name: str, text: str) -> str:
+	""" Injects a note about copying the project config when using the parameter. """
+
+	# print(f"Injecting project config copying notes for {class_name}.{parameter_name}")
+
+	note = None
+
+	match f"{class_name}.{parameter_name}":
+		case "Functionality.version" | "Functionality.license" | "Functionality.organization":
+			note = f"When the `{parameter_name}` field is left empty in a component's `.functionality` configuration, the value of `.version` in the @[package config](package_config) will be copied during build."
+		case "Config.version" | "Config.license" | "Config.organization":
+			note = f"When the `{parameter_name}` field is left empty in a component's configuration, the value of `.version` in the @[package config](package_config) will be copied during build."
+		case "PackageConfig.version" | "PackageConfig.license" | "PackageConfig.organization":
+			note = f"When the `{parameter_name}` field is left empty in a component's @[configuration](config), the value of `.version` in the package config will be copied during build."
+		case "Functionality.repositories":
+			note = "Any repositories defined under `.repositories` in the @[package config](package_config) will be prepended to the repositories defined in a component's @[configuration](functionality) of the `.functionality.repositories` field."
+		case "Config.repositories":
+			note = "Any repositories defined under `.repositories` in the @[package config](package_config) will be prepended to the repositories defined in a component's @[configuration](config) of the `.repositories` field."
+		case "PackageConfig.repositories":
+			note = "Any repositories defined under `.repositories` in the project config will be prepended to the list of repositories defined in a component's `.repositories` field."
+		case "Links.repository" | "Links.docker_registry":
+			note = f"When the `{parameter_name}` field is left empty in a component's `.links` @[configuration](config), the value of `.links.repository` in the @[package config](package_config) will be copied during build."
+
+	if note is not None:
+		return text + "\n\n:::{.callout-note}\n" + note + "\n:::\n\n"
+
+	return text
+
 def fudge_named_examples(this_info):
 	# print(f"this_info before {this_info}")
 
@@ -114,7 +143,7 @@ def fudge_named_examples(this_info):
 	examples_orig = []
 	for example in examples:
 		ex = example.copy()
-		ex['description'] = "Example without `name` field in case used in `.functionality.dependencies`"
+		ex['description'] = "Example without `name` field in case used in `.dependencies`"
 		# strip name field from the example
 		ex['example'] = re.sub(r"^name:.*\n", "", ex['example'])
 		examples_orig.append(ex)
@@ -122,7 +151,7 @@ def fudge_named_examples(this_info):
 	examples_new = []
 	for example in examples:
 		ex = example.copy()
-		ex['description'] = "Example with `name` field in case used in `.functionality.repositories`"
+		ex['description'] = "Example with `name` field in case used in `.repositories`"
 		examples_new.append(ex)
 
 	this_info['example'] = examples_orig + examples_new
@@ -130,9 +159,9 @@ def fudge_named_examples(this_info):
 
 def patch_repository(info: list) -> list:
 	descr = """:::{.callout-warning}
-When defining repositories under `.functionality.repositories`, the repository definition needs a `name` field so it can be refered to from a dependency.
+When defining repositories under `.repositories`, the repository definition needs a `name` field so it can be refered to from a dependency.
 
-When defining a repository directly in a dependency under `.functionality.dependencies`, the `name` field must be omitted.
+When defining a repository directly in a dependency under `.dependencies`, the `name` field must be omitted.
 :::"""
 
 	# print(f"json_data before {info}")
